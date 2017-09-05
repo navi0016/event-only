257861b9b8e9971009811f4d2563e0e3bde7bb05
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-05 01:50:22 -0400

    set up partial view and apply to edit, index and show

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -4,19 +4,19 @@ class ExampleController < ApplicationController
   end
 
   def create
-    @job_application = JobApplication.new
-
-    @job_application.full_name = params [:job_application][:full_name]
-    @job_application.phone = params[:job_application][:phone]
-    @job_application.hobby = parmas[:job_application][:hobby]
-    @job_application.years_experience =
-    params[:job_application][:years_experience]
-    @job_application.available_date = params[:job_application][:available_date]
-
-    if @job_applications.Save
-      redirect_to 'home/index'
-    else
-      render :new
-    end
+    # @job_application = JobApplication.new
+    #
+    # @job_application.full_name = params [:job_application][:full_name]
+    # @job_application.phone = params[:job_application][:phone]
+    # @job_application.hobby = parmas[:job_application][:hobby]
+    # @job_application.years_experience =
+    # params[:job_application][:years_experience]
+    # @job_application.available_date = params[:job_application][:available_date]
+    #
+    # if @job_applications.Save
+    #   redirect_to 'home/index'
+    # else
+    #   render :new
+    # end
   end
 end
